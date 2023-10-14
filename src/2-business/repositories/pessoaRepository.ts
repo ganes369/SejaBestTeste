39fbff8b1ed74f8ed20d3fab1e35aeb9b375ea68
j.a.s <aras.strong8@gmail.com>
2023-10-14 19:37:59 -0300

    fix: adjust type

--- src/2-business/repositories/pessoaRepository.ts
+++ src/2-business/repositories/pessoaRepository.ts
@@ -2,7 +2,7 @@ import { IPessoaEntity } from "../../1-domain/pessoa"
 
 export interface IPessoaRepository {
   create(props: IPessoaEntity): Promise<IPessoaEntity>
-  findOne(prop: number): Promise<IPessoaEntity | undefined>
+  findOne(prop: number): Promise<IPessoaEntity>
   findName(prop: string): Promise<IPessoaEntity[]>
   findAll(): Promise<IPessoaEntity[]>
   update(props: Omit<IPessoaEntity, "fone"> & { id: number }): Promise<[]>
