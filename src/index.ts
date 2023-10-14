ac966777ee628d4d00329e909d2dbd00768266cb
j.a.s <aras.strong8@gmail.com>
2023-10-14 19:52:21 -0300

    fix: remove body-parse

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from "express"
 import routes from "./routes"
-import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use("/api", routes)
 
 app.listen(port, () => {
